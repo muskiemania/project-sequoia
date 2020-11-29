50c6a896906998777832fe9831e80c4f9bfa59c5
Justin Muskivitch <muskiemania@gmail.com>
2020-11-29 14:27:40 -0600

    pdf hardening index

--- console/helpers/pdf_helpers.py
+++ console/helpers/pdf_helpers.py
@@ -6,6 +6,7 @@ import art
 import fpdf
 import requests
 import textwrap
+import roman
 
 class PDFHelpers:
 
@@ -16,6 +17,8 @@ class PDFHelpers:
         self.__column_number = None
         self.__wrapper = None
         self.__first_chapter = False
+
+        self.__index_start = 0
 
     def init(self):
         self._pdf.set_auto_page_break(False)
@@ -28,6 +31,12 @@ class PDFHelpers:
         self.__first_chapter = True
 
         return self
+
+    def __page_number(self):
+        return self._pdf.page_no() - 1
+
+    def __index_page(self):
+        return roman.toRoman(self._pdf.page_no() - self.__index_start).lower()
 
     def write_chapter(self, chapter, people, _begin_chapter=True):
 
@@ -64,7 +73,7 @@ class PDFHelpers:
                         self._pdf.set_xy(72 * 4.75, 52)
                         self._pdf.multi_cell(72 * 2.75, 10.0, f'...{_this_page[-1]}', 0, 'R')
                         self._pdf.set_xy(72, (10 * 72) + 20)
-                        self._pdf.multi_cell(72 * 6.5, 10.0, f'-- Page {_pdf.page_no()} --', 0, 'C')
+                        self._pdf.multi_cell(72 * 6.5, 10.0, f'-- Page {self.__page_number()} --', 0, 'C')
                     
                         self._pdf.add_page()
                         self.__this_page = []
@@ -80,6 +89,7 @@ class PDFHelpers:
                 self._pdf.multi_cell(72 * 2.75, 10.0, _wrapped_sentence)
                 self.__this_page.append(_wrapped_sentence.split(' ')[0])
                 print(_wrapped_sentence + f' x: {self._pdf.get_x()}, y: {self._pdf.get_y()}')
+                self.__index.append((person.summary, self.__page_number()))
                 _begin_chapter = False
 
             else:
@@ -103,7 +113,7 @@ class PDFHelpers:
                         self._pdf.set_xy(72 * 4.75, 52)
                         self._pdf.multi_cell(72 * 2.75, 10.0, f'...{_this_page[-1]}', 0, 'R')
                         self._pdf.set_xy(72, (10 * 72) + 20)
-                        self._pdf.multi_cell(72 * 6.5, 10.0, f'-- Page {_pdf.page_no()} --', 0, 'C')
+                        self._pdf.multi_cell(72 * 6.5, 10.0, f'-- Page {self.__page_number()} --', 0, 'C')
  
                         self._pdf.add_page()
                         self.__this_page = []
@@ -116,9 +126,9 @@ class PDFHelpers:
                 self._pdf.set_xy(72 if self.__column_number == 1 else 72 * 4.75, self._pdf.get_y() + (0 if self._pdf.get_y() == 72 else 10))
                 self._pdf.multi_cell(72 * 2.75, 10.0, _wrapped_sentence)
                 self.__this_page.append(_wrapped_sentence.split(' ')[0])
+                self.__index.append((person.summary, self.__page_number()))
                 print(_wrapped_sentence + f' x: {self._pdf.get_x()}, y: {self._pdf.get_y()}')
  
-
     def complete(self):
         if self.__this_page:
             self._pdf.set_xy(72, 52)
@@ -126,9 +136,86 @@ class PDFHelpers:
             self._pdf.set_xy(72 * 4.75, 52)
             self._pdf.multi_cell(72 * 2.75, 10.0, f'...{self.__this_page[-1]}', 0, 'R')
             self._pdf.set_xy(72, (10 * 72) + 20)
-            self._pdf.multi_cell(72 * 6.5, 10.0, f'-- Page {self._pdf.page_no()} --', 0, 'C')
+            self._pdf.multi_cell(72 * 6.5, 10.0, f'-- Page {self.__page_number()} --', 0, 'C')
  
+        self.__write_index()
+
         self._pdf.output('sample_fpdf.pdf', 'F')
+
+    def __write_index(self):
+
+        self.__index_start = self._pdf.page_no()
+        self._pdf.add_page()
+
+        self._pdf.set_xy(72, 72)
+        self._pdf.multi_cell(72 * 6.5, 10.0, art.text2art('index', font='ogre'))
+        _column_number = 1
+        _current_letter = ''
+
+        for (summary, page_num) in self.__index:
+            if summary[0].upper() != _current_letter:
+                _current_letter = summary[0].upper()
+                # check if room for 1 + subheader + 1 + name
+                if self._pdf.get_y() > ((72 * 10) - (10 * (1 + 1 + 1 + 1))):
+                    print('new section overflows')
+                
+                    if _column_number == 1:
+                        self._pdf.set_xy(4.75 * 72, 72)
+                        _column_number = 2
+                        print('***** SECOND COL *****')
+
+                    elif _column_number == 2:
+                        self._pdf.set_xy(72, (10 * 72) + 20)
+                        self._pdf.multi_cell(72 * 6.5, 10.0, f'-- {self.__index_page()} --', 0, 'C')
+                    
+                        self._pdf.add_page()
+                        self._pdf.set_xy(72, 72)
+                        _column_number = 1
+                        print('***** NEW   PAGE *****')
+                        print('***** FIRST  COL *****')
+
+                self._pdf.set_xy(72 if _column_number == 1 else 72 * 4.75, self._pdf.get_y() + (0 if self._pdf.get_y() == 72 else 10))
+                self._pdf.multi_cell(72 * 2.75, 10.0, f'[{_current_letter.upper()}]')
+                print(_current_letter + f' x: {self._pdf.get_x()}, y: {self._pdf.get_y()}')
+                self._pdf.set_xy(72 if _column_number == 1 else 72 * 4.75, self._pdf.get_y() + 10)
+                
+                _indexed_summary = summary + '  ' + ('.'*(40 - (2 + 2 + len(str(page_num)) + len(summary)))) + ('  ' + str(page_num))
+        
+                self._pdf.multi_cell(72 * 2.75, 10.0, _indexed_summary)
+                print(summary + f' x: {self._pdf.get_x()}, y: {self._pdf.get_y()}')
+
+            elif summary[0].upper() == _current_letter:
+                # check if room for name
+                if self._pdf.get_y() > ((72 * 10) - 10):
+                    print('name overflows')
+                
+                    if _column_number == 1:
+                        self._pdf.set_xy(4.75 * 72, 72)
+                        _column_number = 2
+                        print('***** SECOND COL *****')
+
+                    elif _column_number == 2:
+                        self._pdf.set_xy(72, (10 * 72) + 20)
+                        self._pdf.multi_cell(72 * 6.5, 10.0, f'-- {self.__index_page()} --', 0, 'C')
+                    
+                        self._pdf.add_page()
+                        self._pdf.set_xy(72, 72)
+                        _column_number = 1
+                        print('***** NEW   PAGE *****')
+                        print('***** FIRST  COL *****')
+
+                self._pdf.set_xy(72 if _column_number == 1 else 72 * 4.75, self._pdf.get_y())
+                
+                _indexed_summary = summary + '  ' + ('.'*(40 - (2 + 2 + len(str(page_num)) + len(summary)))) + ('  ' + str(page_num))
+                
+                self._pdf.multi_cell(72 * 2.75, 10.0, _indexed_summary)
+                print(summary + f' x: {self._pdf.get_x()}, y: {self._pdf.get_y()}')
+
+        self._pdf.set_xy(72, (10 * 72) + 20)
+        self._pdf.multi_cell(72 * 6.5, 10.0, f'-- Page {self.__index_page()} --', 0, 'C')
+ 
+        while self._pdf.page_no() % 4 > 0:
+            self._pdf.add_page()
 
     def __write_title_page(self, prepared_for='muskiemania'):
         self._pdf.add_page()
