2d9df554fea9dd635239e84721e7ed650adb191d
Justin Muskivitch <muskiemania@gmail.com>
2020-11-29 22:29:40 -0600

    added venue and ap state abbrev

--- console/helpers/location_helpers.py
+++ console/helpers/location_helpers.py
@@ -1,10 +1,15 @@
 import traceback
+import us
 
 class LocationHelpers:
 
     def __init__(self, data):
     
         self._data = data
+
+    @property
+    def venue(self):
+        return self._data.get('venue')
 
     @property
     def city(self):
@@ -20,6 +25,8 @@ class LocationHelpers:
 
     def load(self, args):
 
+        if args.venue:
+            self._data['venue'] = args.venue
         if args.city:
             self._data['city'] = args.city
         if args.state:
@@ -37,23 +44,47 @@ class LocationHelpers:
 
         _output = ''
 
+        if self.venue:
+            _output += f' at {self.venue}'
+
         if self.city and not self.state and not self.country:
             _output += f' in {self.city}'
 
         if self.city and self.state and not self.country:
-            _output += f' in {self.city}, {self.state}'
+            # for city + state will do city, sta.
+            try:
+                _state = us.states.lookup(self.state).ap_abbr
+            except:
+                _state = self.state
+            _output += f' in {self.city}, {_state}'
 
         if self.city and self.state and self.country:
-            _output += f' in {self.city}, {self.state} ({self.country})'
+            # for city + state + country will do city, sta. (country)
+            try:
+                _state = us.states.lookup(self.state).ap_abbr
+            except:
+                _state = self.state
+            _output += f' in {self.city}, {_state} ({self.country})'
 
         if self.city and not self.state and self.country:
-            _output += f' in {self.city} ({self.country})'
+            # for city + country will do city, cty
+            _output += f' in {self.city} {self.country}'
 
         if not self.city and self.state and not self.country:
-            _output += f' in {self.state}'
+            # for state only, will do sta.
+            try:
+                _state = us.states.lookup(self.state).ap_abbr
+            except:
+                _state = self.state
+            _output += f' in {_state}'
 
         if not self.city and self.state and self.country:
-            _output += f' in {self.state} ({self.country})'
+            # for state country, will do sta., CTY
+            try:
+                _state = us.states.lookup(self.state).ap_abbr
+            except:
+                _state = self.state
+            _output += f' in {_state}, {self.country}'
 
         if not self.city and not self.state and self.country:
             _output += f' in {self.country}'
