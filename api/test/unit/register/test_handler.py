fb762179c488950f055882754ba51685672a722c
EC2 Default User <ec2-user@ip-172-26-4-75.us-east-2.compute.internal>
2022-11-12 04:33:44 +0000

    another unit test for handler

--- api/test/unit/register/test_handler.py
+++ api/test/unit/register/test_handler.py
@@ -6,6 +6,7 @@ def test_handler_ok(mock_register):
     # arrange
     event = {}
     context = {}
+    mock_register.return_value = True
 
     # act
     actual = handler.handler(event, context)
@@ -13,4 +14,19 @@ def test_handler_ok(mock_register):
     # assert
     assert actual['statusCode'] == 200
     assert actual['body'] == 'OK'
+    mock_register.assert_called_once_with(event)
+
+@patch('register.controller.RegistrationController.register')
+def test_handler_error(mock_register):
+    # arrange
+    event = {}
+    context = {}
+    mock_register.return_value = False
+
+    # act
+    actual = handler.handler(event, context)
+
+    # assert
+    assert actual['statusCode'] == 500
+    assert actual['body'] == 'ERROR'
     mock_register.assert_called_once_with(event)
